0dfd3ac424a5ce971493fe510c7312c42e43dcee
Iago Ferreira <iago-ferreira@outlook.com>
2023-09-16 21:17:42 -0300

    feat: adding backend project basic structure

---
+++ backend/LoginSystem.Core/SharedContext/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Core.SharedContext.Entities
+{
+    public abstract class Entity : IEquatable<Guid>
+    {
+        public Guid Id { get; }
+        protected Entity() => Id = Guid.NewGuid();
+
+        bool IEquatable<Guid>.Equals(Guid id) => Id == id;
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
